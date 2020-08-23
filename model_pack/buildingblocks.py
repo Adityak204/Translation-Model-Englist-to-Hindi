5340e38c4d108f07444fb272064184c76c159f87
Aditya Kumar Singh <adityak204@gmail.com>
2020-08-23 17:56:55 +0530

    modulerization + applying linear layer on full embedding size in self attention block

--- model_pack/buildingblocks.py
+++ model_pack/buildingblocks.py
@@ -15,24 +15,30 @@ class SelfAttention(nn.Module):
         """
         assert (self.embed_size % self.heads == 0), "Embed size should be in multiple of heads"
 
-        self.values = nn.Linear(in_features=self.head_dim, out_features=self.head_dim, bias=False)
-        self.keys = nn.Linear(in_features=self.head_dim, out_features=self.head_dim, bias=False)
-        self.queries = nn.Linear(in_features=self.head_dim, out_features=self.head_dim, bias=False)
-        self.fc_out = nn.Linear(in_features=self.head_dim*self.heads, out_features=self.embed_size)
+        # self.values = nn.Linear(in_features=self.head_dim, out_features=self.head_dim, bias=False)
+        # self.keys = nn.Linear(in_features=self.head_dim, out_features=self.head_dim, bias=False)
+        # self.queries = nn.Linear(in_features=self.head_dim, out_features=self.head_dim, bias=False)
+        # self.fc_out = nn.Linear(in_features=self.head_dim*self.heads, out_features=self.embed_size)
+
+        self.values = nn.Linear(in_features=self.embed_size, out_features=self.embed_size, bias=False)
+        self.keys = nn.Linear(in_features=self.embed_size, out_features=self.embed_size, bias=False)
+        self.queries = nn.Linear(in_features=self.embed_size, out_features=self.embed_size, bias=False)
+        self.fc_out = nn.Linear(in_features=self.head_dim * self.heads, out_features=self.embed_size)
 
     def forward(self, values, keys, query, mask=None):
         N = query.shape[0]  # Number of training examples
         value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
 
-        # Splitting embeddings into 'H' heads for creating multi-head attention
-        # V, K, Q reshape = num_samp, seq_len, heads, heads_dim
-        values = values.reshape(N, value_len, self.heads, self.head_dim)
-        keys = keys.reshape(N, key_len, self.heads, self.head_dim)
-        queries = query.reshape(N, query_len, self.heads, self.head_dim)
-
         values = self.values(values)  # (N, value_len, heads, head_dim)
         keys = self.keys(keys)  # (N, key_len, heads, head_dim)
-        queries = self.queries(queries)  # (N, query_len, heads, heads_dim)
+        queries = self.queries(query)  # (N, query_len, heads, heads_dim)
+
+        # Splitting embeddings into 'H' heads for creating multi-head attention
+        # V, K, Q reshape = num_samp, seq_len, heads, heads_dim
+
+        values = values.reshape(N, value_len, self.heads, self.head_dim)
+        keys = keys.reshape(N, key_len, self.heads, self.head_dim)
+        queries = queries.reshape(N, query_len, self.heads, self.head_dim)
 
         # Attention calculation
         # attention score = softmax(Q*t(K))/sqrt(Q.shape[-1])
@@ -51,7 +57,7 @@ class SelfAttention(nn.Module):
             """
             score = score.masked_fill(mask == 0, float('-1e20'))
         attention_score = torch.softmax(score / math.sqrt(self.head_dim), dim=-1)  # N, heads, query_len, key_len
-        out = torch.einsum("nhql,nlhd->nqhd", attention_score, values).view(N, query_len, self.heads*self.head_dim)
+        out = torch.einsum("nhql,nlhd->nqhd", attention_score, values).reshape(N, query_len, self.heads*self.head_dim)
         # out.shape >> N, query_len, embed_size
 
         out = self.fc_out(out)
@@ -85,7 +91,7 @@ class PositionalEncoding(nn.Module):
     PE(pos,2i) = sin(pos/10000^(2i/emb_size))
     PE(cos,2i+1) = cos(pos/10000^(2i+1/emb_size))
     """
-    def __init__(self, max_len, embed_size, dropout):
+    def __init__(self, max_len, embed_size, dropout, device):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(dropout)
 
@@ -95,7 +101,7 @@ class PositionalEncoding(nn.Module):
         div_term = torch.exp(torch.arange(0, embed_size, 2) * -(math.log(10000.0) / embed_size))
         pe[:, 0::2] = torch.sin(position * div_term)  # even place in emb_dim get sin wavelength
         pe[:, 1::2] = torch.cos(position * div_term)  # odd place in emb_dim get cos wavelength
-        pe = pe.unsqueeze(0)
+        pe = pe.unsqueeze(0).to(device)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
@@ -136,15 +142,21 @@ Action Replay : Addition of position encoding with word embedding
 """
 
 
-class Encoder(nn.Module):
-    def __init__(self, src_vocab_size, embed_size, num_layers, heads, device, forward_expansion, dropout, max_len):
-        super(Encoder, self).__init__()
-        self.embed_size = embed_size
-        self.device = device
-        self.word_embedding = nn.Embedding(src_vocab_size, embed_size)
-        self.position_embedding = PositionalEncoding(max_len, embed_size, dropout)
+class DecoderBlock(nn.Module):
+    def __init__(self, embed_size, heads, forward_expansion, dropout, device):
+        super(DecoderBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm = nn.LayerNorm(embed_size)
+        self.transformer_block = TransformerBlock(embed_size=embed_size,
+                                                  heads=heads,
+                                                  dropout=dropout,
+                                                  forward_expansion=forward_expansion)
+        self.dropout = nn.Dropout(dropout)
 
-
-
+    def forward(self, x, value, key, trg_mask, src_mask=None):
+        attention = self.attention(x, x, x, mask=trg_mask)
+        query = self.dropout(self.norm(attention + x))  # LayerNorm + Skip connection
+        out = self.transformer_block(value=value, key=key, query=query, mask=src_mask)
+        return out
 
 
